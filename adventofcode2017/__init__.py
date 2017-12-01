d391ea821b29f2ebdc769b3bce207512fcd5d5c9
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-01 17:55:02 +0100

    Add func to download input

---
+++ adventofcode2017/__init__.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+
+session = os.getenv('AOC_SESSION')
+
+url = 'http://adventofcode.com/2017/day/{}/input'
+
+
+def get_input(day):
+
+    r = requests.get(url.format(day), cookies={'session': session})
+    r.raise_for_status()
+
+    return r.text.strip()
---
+++ tests/test_download.py
@@ -0,0 +1,4 @@
+def test_download():
+    from adventofcode2017 import get_input
+
+    get_input(1)
