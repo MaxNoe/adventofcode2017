9507931b63df01f7a40ae48c3033375d75f522fe
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-01 17:58:41 +0100

    Solve second puzzle

--- adventofcode2017/day1.py
+++ adventofcode2017/day1.py
@@ -12,12 +12,25 @@ def solve_captcha(numbers):
     return s
 
 
+def solve_captcha_2(numbers):
+
+    l = len(numbers)
+    double = numbers + numbers
+
+    s = 0
+    for i, n in enumerate(numbers):
+        if n == double[l // 2 + i]:
+            s += n
+    return s
+
+
 def main():
     my_input = get_input(1)
 
     numbers = list(map(int, my_input.strip()))
 
     print('Task 1:', solve_captcha(numbers))
+    print('Task 2:', solve_captcha_2(numbers))
 
 
 
