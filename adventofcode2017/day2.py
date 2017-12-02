a418178af763191f3497f754b6800bad9a593fba
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-02 13:20:54 +0100

    Add solution for day 2, puzzle 1

---
+++ adventofcode2017/day2.py
@@ -0,0 +1,26 @@
+from . import get_input
+
+
+def calc_row_checksum(row):
+    return max(row) - min(row)
+
+
+def calc_checksum(spreadsheet):
+    return sum(map(calc_row_checksum, spreadsheet))
+
+
+def parse_spreadsheet(text):
+    return [
+        [int(cell) for cell in row]
+        for row in text.splitlines()
+    ]
+
+
+def main():
+    my_input = get_input(2)
+    spreadsheet = parse_spreadsheet(my_input)
+    print('Task 1:', calc_checksum(spreadsheet))
+
+
+if __name__ == '__main__':
+    main()
