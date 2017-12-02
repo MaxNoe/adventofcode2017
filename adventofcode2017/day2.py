00d34e19de95815324fc796fd5bcc4697cf8dbe4
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-02 13:34:32 +0100

    Add solution for day 2, puzzle 2

--- adventofcode2017/day2.py
+++ adventofcode2017/day2.py
@@ -5,8 +5,17 @@ def calc_row_checksum(row):
     return max(row) - min(row)
 
 
-def calc_checksum(spreadsheet):
-    return sum(map(calc_row_checksum, spreadsheet))
+def calc_row_checksum_division(row):
+    row = sorted(row, reverse=True)
+    for i, num in enumerate(row):
+        for other in row[i + 1:]:
+            if num % other == 0:
+                return num / other
+    raise ValueError('No even division found')
+
+
+def calc_checksum(spreadsheet, func=calc_row_checksum):
+    return sum(map(func, spreadsheet))
 
 
 def parse_spreadsheet(text):
@@ -19,7 +28,9 @@ def parse_spreadsheet(text):
 def main():
     my_input = get_input(2)
     spreadsheet = parse_spreadsheet(my_input)
+
     print('Task 1:', calc_checksum(spreadsheet))
+    print('Task 2:', calc_checksum(spreadsheet, calc_row_checksum_division))
 
 
 if __name__ == '__main__':
