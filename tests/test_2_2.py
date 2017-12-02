ccc667d915e48efdda6f8a1bab27af53bc49b3e0
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-02 13:48:14 +0100

    Also test input parsing

--- tests/test_2_2.py
+++ tests/test_2_2.py
@@ -1,4 +1,9 @@
-my_input = [
+my_input = '''5	9	2	8
+9	4	7	3
+3	8	6	5
+'''.strip()
+
+spreadsheet = [
     [5, 9, 2, 8],
     [9, 4, 7, 3],
     [3, 8, 6, 5],
@@ -6,14 +11,20 @@ my_input = [
 
 
 def test_1():
-    from adventofcode2017.day2 import calc_row_checksum_division
+    from adventofcode2017.day2 import parse_spreadsheet
 
-    assert calc_row_checksum_division(my_input[0]) == 4
-    assert calc_row_checksum_division(my_input[1]) == 3
-    assert calc_row_checksum_division(my_input[2]) == 2
+    assert parse_spreadsheet(my_input) == spreadsheet
 
 
 def test_2():
+    from adventofcode2017.day2 import calc_row_checksum_division
+
+    assert calc_row_checksum_division(spreadsheet[0]) == 4
+    assert calc_row_checksum_division(spreadsheet[1]) == 3
+    assert calc_row_checksum_division(spreadsheet[2]) == 2
+
+
+def test_3():
     from adventofcode2017.day2 import calc_row_checksum_division, calc_checksum
 
-    assert calc_checksum(my_input, calc_row_checksum_division) == 9
+    assert calc_checksum(spreadsheet, calc_row_checksum_division) == 9
