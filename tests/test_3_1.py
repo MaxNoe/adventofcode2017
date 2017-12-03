50e404f27e6b30c70b719b7e6b34fe8104dc22fd
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-03 17:08:25 +0100

    More tests

--- tests/test_3_1.py
+++ tests/test_3_1.py
@@ -5,3 +5,14 @@ def test_manhattan():
     assert manhattan_spiral(12) == 3
     assert manhattan_spiral(23) == 2
     assert manhattan_spiral(1024) == 31
+
+
+def test_spiral():
+    from adventofcode2017.day3 import address2ring
+
+    assert address2ring(1) == 0
+    assert address2ring(2) == 1
+    assert address2ring(9) == 1
+    assert address2ring(10) == 2
+    assert address2ring(25) == 2
+    assert address2ring(26) == 3
