951faed3b28676ec015f17eba46bfaa431bc64fe
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-04 09:34:11 +0100

    Add solution for day 4, puzzle 2

--- adventofcode2017/day4.py
+++ adventofcode2017/day4.py
@@ -1,10 +1,16 @@
 from . import get_input
 
+from functools import partial
 
-def is_valid_password(password):
+
+def is_valid_password(password, anagrams=True):
 
     words = password.split()
-    unique_words = set(words)
+
+    if anagrams is True:
+        unique_words = set(words)
+    else:
+        unique_words = set(map(tuple, map(sorted, words)))
 
     return len(unique_words) == len(words)
 
@@ -16,6 +22,10 @@ def main():
     n_valid = sum(map(is_valid_password, passwords))
 
     print('Task 1:', n_valid)
+
+    is_valid_no_anagrams = partial(is_valid_password, anagrams=False)
+    n_valid_no_anagrams = sum(map(is_valid_no_anagrams, passwords))
+    print('Task 2:', n_valid_no_anagrams)
 
 
 if __name__ == '__main__':
