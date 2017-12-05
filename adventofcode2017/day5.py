841d327ac5eb25882475a7f4cd14bcf25af5163d
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-05 10:08:05 +0100

    Implement solution of day 5, puzzle 1

--- adventofcode2017/day5.py
+++ adventofcode2017/day5.py
@@ -5,6 +5,26 @@ def parse_input(inp):
     return list(map(int, inp.split()))
 
 
+def do_step(position, instructions):
+    instructions = instructions.copy()
+
+    steps = instructions[position]
+    instructions[position] += 1
+    position += steps
+
+    return position, instructions
+
+
+def count_steps(instructions):
+    position = 0
+
+    steps = 0
+    while position < len(instructions):
+        position, instructions = do_step(position, instructions)
+        steps += 1
+    return steps
+
+
 def main():
     my_input = get_input(5)
     instructions = parse_input(my_input)
--- tests/test_5_1.py
+++ tests/test_5_1.py
@@ -13,37 +13,37 @@ def test_parse_input():
     assert parse_input(inp) == [0, 3, 0, 1, -3]
 
 
-def test_jumps():
+def test_steps():
     from adventofcode2017.day5 import parse_input, do_step
 
     instructions = parse_input(inp)
 
     position = 0
-    do_step(position, instructions)
+    position, instructions = do_step(position, instructions)
 
     assert position == 0, 'Wrong position after step 1'
     assert instructions[0] == 1, 'Wrong instruction set after step 1'
 
-    do_step(position, instructions)
+    position, instructions = do_step(position, instructions)
     assert position == 1, 'Wrong position after step 2'
     assert instructions[0] == 2, 'Wrong instruction set after step 2'
 
-    do_step(position, instructions)
+    position, instructions = do_step(position, instructions)
     assert position == 4, 'Wrong position after step 3'
     assert instructions[1] == 4, 'Wrong instruction set after step 3'
 
-    do_step(position, instructions)
+    position, instructions = do_step(position, instructions)
     assert position == 1, 'Wrong position after step 4'
     assert instructions[4] == -2, 'Wrong instruction set after step 4'
 
-    do_step(position, instructions)
+    position, instructions = do_step(position, instructions)
     assert position == 5, 'Wrong position after step 5'
     assert instructions[1] == 5, 'Wrong instruction set after step 5'
 
 
-def test_count_jumps():
-    from adventofcode2017.day5 import parse_input, count_jumps
+def test_count_steps():
+    from adventofcode2017.day5 import parse_input, count_steps
 
     instructions = parse_input(inp)
 
-    assert count_jumps(instructions) == 5
+    assert count_steps(instructions) == 5
