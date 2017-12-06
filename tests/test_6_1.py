3660da5113de32de75d6c75ac4a80be44350a954
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-06 15:47:31 +0100

    Add tests for day 6, puzzle 1

--- tests/test_6_1.py
+++ tests/test_6_1.py
@@ -1,6 +1,33 @@
+nums = [0, 2, 7, 0]
+
+
 def test_parse_input():
     from adventofcode2017.day6 import parse_input
 
-    nums = [0, 2, 7, 0]
     inp = '\t'.join(map(str, nums))
     assert parse_input(inp) == nums
+
+
+def test1():
+    from adventofcode2017.day6 import redistribute_blocks
+
+    result = redistribute_blocks(nums)
+    assert result == [2, 4, 1, 2]
+
+    result = redistribute_blocks(nums)
+    assert result == [3, 1, 2, 3]
+
+    result = redistribute_blocks(nums)
+    assert result == [0, 2, 3, 4]
+
+    result = redistribute_blocks(nums)
+    assert result == [1, 3, 4, 1]
+
+    result = redistribute_blocks(nums)
+    assert result == [2, 4, 1, 2]
+
+
+def test2():
+    from adventofcode2017.day6 import count_cycles
+
+    assert count_cycles(nums) == 5
