0f4c40f0478e911a13cc671d473ad5791d36f884
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-07 13:59:34 +0100

    Add solution for day 7, puzzle 2

--- adventofcode2017/day7.py
+++ adventofcode2017/day7.py
@@ -1,6 +1,6 @@
 from . import get_input
 import re
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 
 tower_re = re.compile('^([a-z]+) [(]([0-9]+)[)](?: -> ([a-z, ]+))?$')
 
@@ -42,13 +42,56 @@ def find_children(root, programs):
     return tree
 
 
+def calc_tree_weight(root, tree, programs):
+    root = programs[root]
+    weight = root.weight
+    for child in tree:
+        weight += calc_tree_weight(child, tree[child], programs)
+    return weight
+
+
+def find_wrong_tree(tree, programs):
+    weights = defaultdict(list)
+
+    for root, subtree in tree.items():
+        weight = calc_tree_weight(root, subtree, programs)
+        weights[weight].append(root)
+
+    for res in filter(lambda t: len(t[1]) == 1, weights.items()):
+        return res[1][0]
+    return None
+
+
+def find_corrected_weight(tree, programs):
+
+    wrong = find_wrong_tree(tree, programs)
+    while wrong:
+        last_tree = tree
+        last_wrong = wrong
+
+        tree = tree[wrong]
+        wrong = find_wrong_tree(tree, programs)
+
+    for key in last_tree:
+        if key != last_wrong:
+            break
+
+    correct_weight = calc_tree_weight(key, last_tree[key], programs)
+    wrong_weight = calc_tree_weight(last_wrong, last_tree[last_wrong], programs)
+
+    return programs[last_wrong].weight - (wrong_weight - correct_weight)
+
+
 def main():
     my_input = get_input(7)
     programs = parse_input(my_input)
 
     print('Task 1:', find_root(programs).name)
 
+    tree = build_tree(programs)
+
+    print('Task 2:', find_corrected_weight(tree, programs))
+
 
 if __name__ == '__main__':
     main()
-
