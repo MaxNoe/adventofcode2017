26097e823b94b93d339481e3b527e05ee0dd6862
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-08 14:43:55 +0100

    Add test for input parsing

---
+++ tests/test_8.py
@@ -0,0 +1,20 @@
+inp = '''b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10
+'''
+
+parsed = [
+    ('b', 'inc', 5, ('a', '>', 5)),
+    ('a', 'inc', 1, ('b', '<', 5)),
+    ('c', 'dec', -10, ('a', '>=', 1)),
+    ('c', 'inc', -20, ('c', '==', 10)),
+]
+
+
+def test_parse_input():
+    from adventofcode2017.day8 import parse_input
+
+    assert parse_input(inp) == parsed
+
+
