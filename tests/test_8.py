a6424adbba4b53c75e3d0589d77e24290abe6b6a
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-08 14:53:40 +0100

    Add tests for task 1

--- tests/test_8.py
+++ tests/test_8.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 inp = '''b inc 5 if a > 1
 a inc 1 if b < 5
 c dec -10 if a >= 1
@@ -18,3 +21,19 @@ def test_parse_input():
     assert parse_input(inp) == parsed
 
 
+def test_instructions():
+    from adventofcode2017.day8 import apply_instruction
+
+    registers = defaultdict(int)
+
+    apply_instruction(parsed[0], registers)
+    assert registers['b'] == 0
+
+    apply_instruction(parsed[1], registers)
+    assert registers['a'] == 1
+
+    apply_instruction(parsed[2], registers)
+    assert registers['c'] == 10
+
+    apply_instruction(parsed[3], registers)
+    assert registers['c'] == -10
