438c7a2a4a48c8188a15790c205ac5be62774b51
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-10 12:39:26 +0100

    Add tests for puzzle 2

--- tests/test_10.py
+++ tests/test_10.py
@@ -34,3 +34,12 @@ def test_do_twist():
     assert elements == [3, 4, 2, 1, 0]
     assert index == 4
     assert skip_size == 4
+
+
+def test_hash():
+    from adventofcode2017.day10 import elf_hash
+
+    assert elf_hash('') == 'a2582a3a0e66e6e86e3812dcb672a272'
+    assert elf_hash('AoC 2017') == '33efeb34ea91902bb2f59c9920caa6cd'
+    assert elf_hash('1,2,3') == '3efbe78a8d82f29979031a4aa0b16a9d'
+    assert elf_hash('1,2,4') == '63960835bcdc130f0b66d7ff4f6a5a8e'
