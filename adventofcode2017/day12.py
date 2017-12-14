7b589a916f0ece4f8f37943654187439cb00394d
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-14 20:50:59 +0100

    Add solution for day 12, puzzle 1

--- adventofcode2017/day12.py
+++ adventofcode2017/day12.py
@@ -11,10 +11,24 @@ def parse_input(inp):
     return dict(map(parse_line, inp.splitlines()))
 
 
+def find_group(root, pipes, group=None):
+    if group is None:
+        group = set()
+    group.add(root)
+    for connected in pipes[root]:
+        if connected not in group:
+            for g in find_group(connected, pipes, group):
+                group.add(g)
+
+    return group
+
+
 def main():
     inp = get_input(12)
-
     pipes = parse_input(inp)
+
+    group_zero = find_group(0, pipes)
+    print('Task 1:', len(group_zero))
 
 
 if __name__ == "__main__":
--- tests/test_12.py
+++ tests/test_12.py
@@ -22,3 +22,9 @@ def test_parse_input():
     from adventofcode2017.day12 import parse_input
 
     assert parse_input(inp) == pipes
+
+
+def test_group():
+    from adventofcode2017.day12 import find_group
+
+    assert find_group(0, pipes) == {0, 2, 3, 4, 5, 6}
