b354f798d0849ff323a043d9df6b6781aa95173f
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-18 10:42:22 +0100

    Solve puzzle 2

--- adventofcode2017/day18.py
+++ adventofcode2017/day18.py
@@ -1,5 +1,6 @@
 from . import get_input
 from collections import defaultdict
+from queue import Queue, Empty
 
 
 def parse_input(inp):
@@ -56,12 +57,70 @@ def get_first_recovered_freq(instructions):
     return last_played
 
 
+def send_recv(instructions):
+    queues = (Queue(), Queue())
+
+    finished = [False, False]
+    waiting = [False, False]
+    n_sent = [0, 0]
+
+    positions = [0, 0]
+    registers = [defaultdict(int), defaultdict(int)]
+    registers[1]['p'] = 1
+
+    while not (all(waiting) or all(finished)):
+
+        for program in [1, 0]:
+            if positions[program] < 0 or positions[program] > len(instructions):
+                finished[program] = True
+
+            instruction, args = instructions[positions[program]]
+            args = args.copy()
+
+            register = registers[program]
+
+            if len(args) > 1 and isinstance(args[1], str):
+                args[1] = register[args[1]]
+
+            if instruction == 'set':
+                register[args[0]] = args[1]
+            elif instruction == 'add':
+                register[args[0]] += args[1]
+            elif instruction == 'mul':
+                register[args[0]] *= args[1]
+            elif instruction == 'mod':
+                register[args[0]] = register[args[0]] % args[1]
+
+            elif instruction == 'rcv':
+                try:
+                    register[args[0]] = queues[program].get_nowait()
+                    waiting[program] = False
+                except Empty:
+                    waiting[program] = True
+                    continue
+
+            if isinstance(args[0], str):
+                args[0] = register[args[0]]
+
+            if instruction == 'snd':
+                queues[not program].put(args[0])
+                n_sent[program] += 1
+
+            if instruction == 'jgz' and args[0] > 0:
+                positions[program] += args[1]
+                continue
+
+            positions[program] += 1
+
+    return n_sent[1]
+
+
 def main():
     inp = get_input(18)
     instructions = parse_input(inp)
 
     print('Task 1:', get_first_recovered_freq(instructions))
-
+    print('Task 2:', send_recv(instructions))
 
 
 if __name__ == "__main__":
