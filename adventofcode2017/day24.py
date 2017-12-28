4298c54ad0620abaeab7b1862d3f1c9cb8a5f010
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-28 11:41:41 +0100

    Solve day 24, puzzle 1

---
+++ adventofcode2017/day24.py
@@ -0,0 +1,47 @@
+from . import get_input
+
+
+def main():
+    components = parse_input(get_input(24))
+
+    bridges = build_bridges(components)
+    best_bridge = find_best_bridge(bridges)
+    print('Task 1:', calc_strength(best_bridge))
+
+
+def parse_input(inp):
+    return list(map(
+        lambda s: tuple(map(int, s.split('/'))),
+        inp.splitlines()
+    ))
+
+
+def calc_strength(bridge):
+    return sum(map(sum, bridge))
+
+
+def build_bridges(components, open_connection=0):
+    bridges = []
+    orig_open = open_connection
+
+    for component in components:
+        open_connection = orig_open
+        if open_connection in component:
+            open_connection = component[not component.index(open_connection)]
+            bridge = [component]
+            bridges.append(bridge)
+
+            new_comps = components[:]
+            new_comps.remove(component)
+            subcomps = build_bridges(new_comps, open_connection)
+            for subcomp in subcomps:
+                bridges.append(bridge + subcomp)
+    return bridges
+
+
+def find_best_bridge(bridges):
+    return sorted(bridges, key=calc_strength)[-1]
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_24.py
@@ -0,0 +1,62 @@
+inp = '''0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10
+'''
+
+components = [
+    (0, 2),
+    (2, 2),
+    (2, 3),
+    (3, 4),
+    (3, 5),
+    (0, 1),
+    (10, 1),
+    (9, 10),
+]
+
+bridges = [
+    ((0, 1), ),
+    ((0, 1), (10, 1)),
+    ((0, 1), (10, 1), (9, 10)),
+    ((0, 2), ),
+    ((0, 2), (2, 3)),
+    ((0, 2), (2, 3), (3, 4)),
+    ((0, 2), (2, 3), (3, 5)),
+    ((0, 2), (2, 2)),
+    ((0, 2), (2, 2), (2, 3)),
+    ((0, 2), (2, 2), (2, 3), (3, 4)),
+    ((0, 2), (2, 2), (2, 3), (3, 5)),
+]
+
+
+def test_parse():
+    from adventofcode2017.day24 import parse_input
+    assert parse_input(inp) == components
+
+
+def test_strength():
+    from adventofcode2017.day24 import calc_strength
+
+    bridge = [(0, 3), (3, 7), (7, 4)]
+    assert calc_strength(bridge) == 24
+
+
+def test_valid_bridges():
+    from adventofcode2017.day24 import build_bridges
+    my_bridges = build_bridges(components)
+
+    my_bridges = set(map(tuple, my_bridges))
+    assert my_bridges == set(bridges)
+
+
+def test_best_bridge():
+    from adventofcode2017.day24 import find_best_bridge, calc_strength, build_bridges
+
+    bridges = build_bridges(components)
+    assert find_best_bridge(bridges) == [(0, 1), (10, 1), (9, 10)]
+    assert calc_strength(find_best_bridge(bridges)) == 31
